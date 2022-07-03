9a5b752f2448815b6525306215dae5a623ebe92a
Сигида Анна <a.sigida@systtech.ru>
2022-07-03 12:11:22 +0200

    add game

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print('hello')
+import random
+
+
+def game():
+    print('Я хочу сыграть с тобой в игру ;]')
+    guessing_number = guessing(random.randint(1, 10))
+    print('Молодец! Ты угадал!!!')
+
+
+def guessing(guessing_number):
+    while True:
+        try:
+            user_number = int(input('Попробуй угадать число от 1 до 10: '))
+            if guessing_number == user_number:
+                return
+
+            if guessing_number > user_number:
+                print('Загаданное число больше\n')
+
+            if guessing_number < user_number:
+                print('Загаданное число меньше\n')
+
+        except ValueError:
+            print('Ты ввел не число, не надо так...')
+
+
+if __name__ == '__main__':
+    game()
