153cc515a94f6370d0eafa7f1cf92762d7edc5e1
Anya <a.sigida@systtech.ru>
2022-07-04 18:56:12 +0200

    update

--- Engine.py
+++ Engine.py
@@ -5,9 +5,14 @@ class Engine():
 
     def __init__(self):
         self.guessing_from = 1
-        self.guessing_to = 10
+        self.guessing_to = 100
         self.guessing_int = randint(self.guessing_from, self.guessing_to)
         self.start()
+
+
+
+
+
 
 
 
--- SecondGame.py
+++ SecondGame.py
@@ -4,12 +4,13 @@ from Engine import Engine
 class SecondGame(Engine):
 
     def start(self):
+
         while True:
             first_try = ((self.guessing_from + self.guessing_to) // 2)
             user_answer = input(f'Число больше, меньше или равно {first_try}? ')
 
             if user_answer == '=':
-                print('Это было легко')
+                print(f'Это было легко..твое число {first_try}')
                 break
 
             elif user_answer == '>':
@@ -23,6 +24,7 @@ class SecondGame(Engine):
             else:
                 print('Чеее??')
                 break
+
 
         self.start()
 
